46baed8284ce568c825ee67abcfa7c62ed697e47
Cavdy <cavdyofficials@gmail.com>
2019-12-17 11:02:18 +0100

    Setup DB, Yoga and Prisma

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-// let's go!
+require('dotenv').config({ path: '.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// TODO Use express middlewares to handle cookies (JWT)
+// TODO Use express middlewares to populate current user
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  deets => {
+    console.log(`Server is now running on http://localhost:${deets.port}`);
+  }
+);
