d4841d9b85084a53e2bd23b3a3fb4c9a91f7cb14
Cavdy <cavdyofficials@gmail.com>
2019-12-18 18:28:13 +0100

    Update Item

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -3,6 +3,16 @@ const Mutations = {
     // TODO: Check if they are logged in
     const item = await ctx.db.mutation.createItem({ data: { ...args } }, info);
     return item;
+  },
+  async updateItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+    const update = { ...args };
+    delete update.id;
+    const item = await ctx.db.mutation.updateItem(
+      { data: update, where: { id: args.id } },
+      info
+    );
+    return item;
   }
 };
 
